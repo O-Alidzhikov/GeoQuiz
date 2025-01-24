707fdbfff71e3aace2b912aee35506bfa08a43ff
O-Alidzhikov <oki30g@gmail.com>
2025-01-24 14:07:52 +0200

    added the map of Asia without logic applied to it

--- GeographyQuiz/src/App.jsx
+++ GeographyQuiz/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import AuthProvider from './contexts/userContext';
 import Header from './components/header/home/header'
 import Quiz from './components/eu/eu'
+import Asia from './components/asia/asia';
 import Login from './components/login/login'
 import Register from './components/register/register';
 import Logout from './components/logout/logout';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/logout" element={<Logout />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/quiz" element={<Quiz />}></Route>
+          <Route path="/asia" element={<Asia />}></Route>
           <Route path="/quizzes" element={<Quizes />}></Route>
         </Routes>
       </AuthProvider>
